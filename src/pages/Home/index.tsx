ba394017c411695a8b342406364a21421e4a6d42
Thiago Lourenço <tlcb@ecomp.poli.br>
2021-05-24 18:11:23 -0300

    Change App to Home page

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../../components/Header';
+
+function Home() {
+  return (
+    <div className="App">
+      <Header/>
+      <h1>HOME PAGE</h1>
+    </div>
+  );
+}
+
+export default Home;
