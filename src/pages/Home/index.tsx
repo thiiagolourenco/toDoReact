e22f2f0d957ea9ab8230ffbaaafdd12297b667d6
Thiago Lourenço <tlcb@ecomp.poli.br>
2021-05-25 18:28:41 -0300

    Add component Filter and add hook

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
+import FilterCard from '../../components/FilterCard';
+
+import styles from './styles.module.scss';
 
 function Home() {
+  const [isActive, setIsActive] = useState('');
+  
+  function changeFilterState(filterTitle: string){
+    setIsActive(filterTitle);
+  };
+
   return (
     <div>
       <Header/>
-      <h1>HOME PAGE</h1>
+      <h2>Filtros</h2>
+      <div className={styles.cardContainer}>
+        <FilterCard title="Todos" isActive={isActive === 'all'} changingColor={() => changeFilterState('all')}/>
+        <FilterCard title="Dia" isActive={isActive === 'day'} changingColor={() => changeFilterState('day')}/>
+        <FilterCard title="Semana" isActive={isActive === 'week'} changingColor={() => changeFilterState('week')}/>
+        <FilterCard title="Mês" isActive={isActive === 'month'} changingColor={() => changeFilterState('month')}/>
+      </div>
       <Footer/>
     </div>
   );
