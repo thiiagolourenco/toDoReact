fc94930bc90347e22159af6c5e3bd2d6e79d960c
Thiago Lourenço <tlcb@ecomp.poli.br>
2021-05-24 21:09:47 -0300

    Add new styles

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
-import logo from "../../assets/logo.png"; 
+
+import logo from "../../assets/logo.png";
+import bell from "../../assets/bell.png"; 
+
 import styles from "./styles.module.scss";
 
 function Header() {
@@ -7,9 +10,15 @@ function Header() {
     <header className={styles.headerContainer}>
       <img src={logo} alt="Logo do ToDo"></img>
       <ul>
-        <li><a>Início</a></li>
-        <li><a>Nova Tarefa</a></li>
-        <li><a>Sincronizar</a></li>
+        <li><a href="">Início</a></li>
+        <li className={styles.leftBorder}><a href="">Nova Tarefa</a></li>
+        <li className={styles.leftBorder}><a href="">Sincronizar</a></li>
+        <li className={styles.leftBorder}>
+          <div className={styles.notification}>
+            <img src={bell} alt="Icone de notificação"></img>
+            <span>5</span>  
+          </div>
+        </li>
       </ul>
     </header>
   );
