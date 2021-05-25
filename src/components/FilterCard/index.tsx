2292c4805911f5966725def78e1569e13da48df1
Thiago Lourenço <tlcb@ecomp.poli.br>
2021-05-25 18:27:54 -0300

    Add hook and style to the component

--- src/components/FilterCard/index.tsx
+++ src/components/FilterCard/index.tsx
@@ -1,10 +1,29 @@
 import React from 'react';
 
 import styles from "./styles.module.scss";
+import filter from "../../assets/filter.png";
 
-function FilterCard() {
+type PropsFilter = {
+  title: string,
+  isActive: boolean,
+  changingColor: () => void
+}
+
+function FilterCard({title, isActive, changingColor}: PropsFilter) {
+  
   return (
-    <div></div>
+    <div className={styles.homepage}>
+      <button type="button" className={styles.latestEpisodes} onClick={changingColor}>
+        <ul>
+          <li className={isActive ? styles.isActive : ''}>
+            <div className={styles.episodeDetails}>
+              <img src={filter} alt="Ícone de filtro"/>
+              <p>{title}</p>
+            </div>
+          </li>
+        </ul>
+      </button>
+    </div>
   );
 }
 
