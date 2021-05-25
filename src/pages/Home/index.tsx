b3da3e5d9cfa6a46996052e58f5204516825b51c
Thiago Lourenço <tlcb@ecomp.poli.br>
2021-05-24 22:26:58 -0300

    Add footer

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../../components/Header';
+import Footer from '../../components/Footer';
 
 function Home() {
   return (
     <div>
       <Header/>
       <h1>HOME PAGE</h1>
+      <Footer/>
     </div>
   );
 }
