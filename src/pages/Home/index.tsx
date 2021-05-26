c4c7f5eecd5b3333983fc4b44d34e79a4701e0e7
Thiago Lourenço <tlcb@ecomp.poli.br>
2021-05-26 00:03:28 -0300

    Change Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
+import TaskCard from '../../components/TaskCard';
 import FilterCard from '../../components/FilterCard';
 
 import styles from './styles.module.scss';
@@ -22,6 +23,46 @@ function Home() {
         <FilterCard title="Dia" isActive={isActive === 'day'} changingColor={() => changeFilterState('day')}/>
         <FilterCard title="Semana" isActive={isActive === 'week'} changingColor={() => changeFilterState('week')}/>
         <FilterCard title="Mês" isActive={isActive === 'month'} changingColor={() => changeFilterState('month')}/>
+      </div>
+      <h2>Tarefas</h2>
+      <div className={styles.cardContainer}>
+        <TaskCard allTasks={[
+          {
+            id: 1,
+            title: 'Estudar Matemática',
+            person: 'Júlia',
+            date: '30/06/2021',
+            hour: '14:00'
+          },
+          {
+            id: 2,
+            title: 'Jogar bola',
+            person: 'Thiago',
+            date: '28/05/2021',
+            hour: '18:00'
+          },
+          {
+            id: 3,
+            title: 'Estuda podologia',
+            person: 'Brenda',
+            date: '25/05/2021',
+            hour: '10:00'
+          },
+          {
+            id: 4,
+            title: 'Comprar pão',
+            person: 'João',
+            date: '28/06/2021',
+            hour: '16:00'
+          },
+          {
+            id: 5,
+            title: 'Fazer a feira',
+            person: 'Hugão',
+            date: '28/06/2021',
+            hour: '16:00'
+          }
+        ]}/>
       </div>
       <Footer/>
     </div>
